b54e4c3fb867c403ab513d4dd0ab392a2bdbb897
LokeshKumar1779 <kumarlokesh57@gmail.com>
2025-10-02 20:19:32 +0530

    added/modified/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 	
 	public void verifLoginPageTitle() {
 		System.out.println("Login Page title");
+		String username = "username";
+		String password = "password";
+		System.out.println("username " +username +" "+"password" + password);
 	}
 
 }
--- src/tests/HomePageTest.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class HomePageTest {
-	
-		public void homepageTest1() {
-			System.out.println("Home Page Test1..");
-		}
-
-}
